ef0b87b2fa054609e1d7f3e7063d502ba559fea3
WalukaUI <cwaluka@yahoo.com>
2022-06-16 13:27:25 -0500

    renamed variables

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let aa = document.getElementsByClassName("s-widget-spacing-small");
 let parent = document.getElementsByClassName("s-search-results");
 let yy = document.getElementsByClassName("sg-col-inner");
+let searchBar = document.getElementsByClassName(
+  "a-section a-spacing-none s-result-item s-flex-full-width s-widget s-widget-spacing-large"
+);
 
 let newA = [];
+let srch = [];
+
+srch.push(searchBar[3]);
 
 for (let i = 0; i < aa.length; i++) {
   if (aa[i].textContent.includes("More Buying Choices") === true) {
@@ -14,9 +20,8 @@ while (yy[2].firstChild) {
   yy[2].removeChild(yy[2].firstChild);
 }
 
-// let ff=document.createElement("div")
-// yy[2].appendChild(ff)
-
 for (let x of newA) {
   yy[2].appendChild(x);
 }
+
+yy[2].appendChild(srch[0]);
